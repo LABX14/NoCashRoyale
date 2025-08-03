f36be7255b88e6d0c1896be79b9450eada1b3e85
Kaulana Lee <Kaulanalee12@gmail.com>
2025-08-02 19:55:49 -0700

    Imported Art Assets and Quit Button works
    When testing, make sure play scene is set to 2D input which allows you to properly interact with the buttons in the Menu

--- Scene/menu.gd
+++ Scene/menu.gd
@@ -1,1 +1,10 @@
 extends Control
+
+func _ready():
+	pass # Replace with function body
+
+func _on_start_button_pressed():
+	get_tree().change_scene("res://Scene/main_scene.tscn")
+
+func _on_quit_button_pressed()-> void:
+	get_tree().quit();
