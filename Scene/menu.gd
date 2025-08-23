401bfe53907286e5d739a3fe7b3537aa54875c47
Kaulana Lee <Kaulanalee12@gmail.com>
2025-08-22 23:12:07 -0700

    UI appears and can now go into the Main Scene
    Fixed code to make start button in Main Menu go to the Game Scene and Button UI appears on Game Scene

--- Scene/menu.gd
+++ Scene/menu.gd
@@ -4,7 +4,7 @@ func _ready():
 	pass # Replace with function body
 
 func _on_start_button_pressed():
-	get_tree().change_scene("res://Scene/main_scene.tscn")
+	get_tree().change_scene_to_file("res://Scene/main_scene.tscn")
 
 func _on_quit_button_pressed()-> void:
 	get_tree().quit();
